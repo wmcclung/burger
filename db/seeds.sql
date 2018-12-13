87874e3a9a01426b2c2d3ad6181a7bb427ad7c76
Will M <wpmcclung@gmail.com>
2018-12-13 00:08:50 -0600

    adding project

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Krabby Patty', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Big Kahuna', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Owen and Engine Tuesday Special', true);
